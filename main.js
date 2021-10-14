685d24990ca17e671be8123441830043fa4d91c8
LouayLammouchi <lammouchilouay@gmail.com>
2021-10-14 13:43:18 +0100

    colorsrandom

--- main.js
+++ main.js
@@ -4,10 +4,28 @@ function generate_rgb(){
 } 
 
 $("#reset").click(function(){
-    $(".colors").css("color","rgb"+generate_rgb())
+    $("#color1").css("color","rgb"+generate_rgb())
   })
+
+  $("#reset").click(function(){
+    $("#color2").css("color","rgb"+generate_rgb())
+  })
+
+  $("#reset").click(function(){
+    $("#color3").css("color","rgb"+generate_rgb())
+  })
+  $("#reset").click(function(){
+    $("#color4").css("color","rgb"+generate_rgb())
+  })
+  $("#reset").click(function(){
+    $("#color5").css("color","rgb"+generate_rgb())
+  })
+  $("#reset").click(function(){
+    $("#color6").css("color","rgb"+generate_rgb())
+  })
+
+
 
 $("header").css("color","rgb"+generate_rgb()) 
 
 
-$("#color2").css("color","black") 
