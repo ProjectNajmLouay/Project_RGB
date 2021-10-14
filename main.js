c3a0fa03cd7180283457501e5d2df8d9cfa948d8
LouayLammouchi <lammouchilouay@gmail.com>
2021-10-14 15:59:07 +0100

    newArr

--- main.js
+++ main.js
@@ -2,29 +2,50 @@ function generate_rgb(){
     return '('+ Math.floor(Math.random()*266)+ "," + Math.floor(Math.random()*266)+" ," + Math.floor(Math.random()*266) + " )"
 
 } 
-
+  var arrRGB = [] ;
 $("#reset").click(function(){
-    $("#color1").css("color","rgb"+generate_rgb())
+     arrRGB = [] 
+    
+  })
+$("#reset").click(function(){
+    var guess1 = generate_rgb() ;
+    arrRGB.push(guess1)
+     ;
+    $("#color1").css("color","rgb"+guess1);
   })
 
   $("#reset").click(function(){
-    $("#color2").css("color","rgb"+generate_rgb())
+    var guess2 = generate_rgb() ;
+    arrRGB.push(guess2)
+    $("#color2").css("color","rgb"+guess2) ;
   })
 
   $("#reset").click(function(){
-    $("#color3").css("color","rgb"+generate_rgb())
+    var guess3 = generate_rgb() ; 
+    arrRGB.push(guess3) ;
+    $("#color3").css("color","rgb"+guess3)
   })
   $("#reset").click(function(){
-    $("#color4").css("color","rgb"+generate_rgb())
+    var guess4 = generate_rgb() ;
+    arrRGB.push(guess4) ;
+    $("#color4").css("color","rgb"+guess4)
   })
   $("#reset").click(function(){
-    $("#color5").css("color","rgb"+generate_rgb())
+    var guess5 = generate_rgb() ;
+    arrRGB.push(guess5)
+    $("#color5").css("color","rgb"+guess5)
   })
   $("#reset").click(function(){
-    $("#color6").css("color","rgb"+generate_rgb())
+    var guess6 = generate_rgb() ;
+    arrRGB.push(guess6)
+    $("#color6").css("color","rgb"+guess6)
   })
 
-
+ 
+ $("#reset").click(function(){
+  console.log(arrRGB)
+  })
+ 
 
 $("header").css("color","rgb"+generate_rgb()) 
 
